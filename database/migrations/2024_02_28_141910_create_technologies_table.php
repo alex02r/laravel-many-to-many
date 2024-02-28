a72461f70c0dc02e2e67f7ea0a75240a107f5f60
alex02r <alex.giochi02@gmail.com>
2024-02-28 15:26:39 +0100

    [UPGRADE] Definizione della migration technologies

--- database/migrations/2024_02_28_141910_create_technologies_table.php
+++ database/migrations/2024_02_28_141910_create_technologies_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('class_color');
             $table->timestamps();
         });
     }
