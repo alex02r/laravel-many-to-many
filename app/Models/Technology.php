72e63f7a928f1d28a71f8b07f49bfc585639af02
alex02r <alex.giochi02@gmail.com>
2024-02-28 16:07:18 +0100

    [UPGRADE] Aggiunta variabile fillable al model di technology

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'slug', 'class_color'];
     public function projects(){
         return $this->belongsToMany(Technology::class);
     }
