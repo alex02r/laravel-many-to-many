435c36369b8229f761e14c9c473df37b4fb77e4e
alex02r <alex.giochi02@gmail.com>
2024-02-28 15:39:31 +0100

    [UPGRADE] Definizione di TechnologySeeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class TechnologySeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 3; $i++) { 
+            $new_technologies = new Technology();
+            $new_technologies->name = $faker->words(2, true);
+            $new_technologies->slug = Str::slug($new_technologies->name, '-');
+            $new_technologies->class_color = $faker->randomElement(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']);
+            $new_technologies->save();
+        }
     }
 }
